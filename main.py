e42e9db395879cd5a217973da8da2a8d268fcbf3
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2021-08-23 12:29:26 +0000

    say and slash commands

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import time
 import discord.ext
+from discord_slash import SlashCommand, SlashContext
 from discord.utils import get
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions,  CheckFailure, check
@@ -9,7 +10,9 @@ from discord.ext.commands import has_permissions,  CheckFailure, check
 
 client = discord.Client()
 
-client = commands.Bot(command_prefix = '.') #put your own prefix here
+client = commands.Bot(command_prefix = '.') #put 
+slash = SlashCommand(client, sync_commands=True)
+#your own prefix here
 
 @client.event
 async def on_ready():
@@ -21,12 +24,33 @@ async def ping(ctx):
     await ctx.send("pong!") #simple command so that when you type "!ping" the bot will respond with "pong!"
 
 @client.command()
+async def say(ctx, *, text):
+   await ctx.channel.purge(limit=1)
+   await ctx.send(text)
+
+
+
+@slash.slash(name="say", description="make the bot say something")
+async def slashsay(ctx, *, text):
+  await ctx.send(text)
+
+
+@client.command()
 async def kick(ctx, member : discord.Member):
     try:
         await member.kick(reason=None)
         await ctx.send("kicked "+member.mention) #simple kick command to demonstrate how to get and use member mentions
     except:
         await ctx.send("bot does not have the kick members permission!")
+
+@slash.slash(name="Kick", description="kicks a person out")
+async def slashkick(ctx, member : discord.Member):
+    try:
+        await member.kick(reason=None)
+        await ctx.send("kicked "+member.mention) #simple kick command to demonstrate how to get and use member mentions
+    except:
+        await ctx.send("bot does not have the kick members permission!")
+
 
 @client.command()
 async def source(ctx):
