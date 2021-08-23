4fdcb1fb2467534c7694ba76fb3c82cb32dca88c
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2021-08-23 12:57:15 +0000

    join and leave commands

--- main.py
+++ main.py
@@ -70,6 +70,12 @@ async def join(ctx):
     await channel.connect()
     print("i'm in the voice channel")
 
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+    print("i'm out of the voice channel")
+
+
 client.run(os.getenv("TOKEN")) #get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value. 
 #to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
 #enjoy!
